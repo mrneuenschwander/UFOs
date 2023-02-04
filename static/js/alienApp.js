db894bae125c60aa02f71e1f473ea9eef75516d8
mrneuenschwander <116296092+mrneuenschwander@users.noreply.github.com>
2023-02-04 10:55:16 -0700

    Update alienApp.js
    table builds and filters

--- static/js/alienApp.js
+++ static/js/alienApp.js
@@ -20,3 +20,22 @@ function buildTable(data) {
         );
     });
 };
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check for a date entered by user, and fiilter using it
+    if (date) {
+        // apply the date filter to the data
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    // rebuild the table using the new filtered data
+    // NOTE: if no filter is present, the table will be the base objects
+    buildTable(filteredData);
+}
+
+// handle a click on the filter
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table
+buildTable(tableData);
