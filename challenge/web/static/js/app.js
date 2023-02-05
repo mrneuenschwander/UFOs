bd734ef03d69777d9da1df1f9b604231d3e8da80
mrneuenschwander <116296092+mrneuenschwander@users.noreply.github.com>
2023-02-05 12:26:18 -0700

    folder arrangement

--- challenge/web/static/js/app.js
+++ challenge/web/static/js/app.js
@@ -53,7 +53,7 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
